d27681b5e483cbcce2501e9fa4f804289c4fd59c
Jume <jumeb@Jumes-Mac.local>
2024-01-25 17:25:29 +0100

    Reverted the next config file

---
+++ next.config.js
@@ -0,0 +1,12 @@
+// @ts-check
+
+/**
+ * @type {import('next').NextConfig}
+ **/
+const nextConfig = {
+    output: "export",
+    // basePath: '/ntopor.github.io',
+    /* config options here */
+  };
+  
+module.exports = nextConfig;
